cd2adab9b9709b993559e4e02e779a6a4b1a8c3f
Gaurav20068 <gaurav93414@gmail.com>
2026-02-21 21:02:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineconst firebaseConfig = {
-  apiKey: "AIzaSyCi4l9qsnThXeMFZ1xGholvWHmDJrca01A",
-  authDomain: "impromptu-b1aa6.firebaseapp.com",
-  projectId: "impromptu-b1aa6",
-  storageBucket: "impromptu-b1aa6.firebasestorage.app",
-  messagingSenderId: "304404810717",
-  appId: "1:304404810717:web:5516f765310b2930f20434",
-  measurementId: "G-Q997Y75BR7"
-};
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Impromptu-3/', 
+  plugins: [react()],
+  define: {
+    __firebase_config: JSON.stringify({
+      apiKey: "PASTE_YOUR_API_KEY_HERE",
+      authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
+      projectId: "PASTE_YOUR_PROJECT_ID_HERE",
+      storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
+      messagingSenderId: "PASTE_YOUR_SENDER_ID_HERE",
+      appId: "PASTE_YOUR_APP_ID_HERE"
+    }),
     __app_id: JSON.stringify('impromptu-v1')
   }
 })
