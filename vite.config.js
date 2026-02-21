34ed8a5330c3d2fa397e0bdeacdc66a7af060804
Gaurav20068 <gaurav93414@gmail.com>
2026-02-21 20:46:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export default defineconst firebaseConfig = {
   apiKey: "AIzaSyCi4l9qsnThXeMFZ1xGholvWHmDJrca01A",
   authDomain: "impromptu-b1aa6.firebaseapp.com",
   projectId: "impromptu-b1aa6",
@@ -16,6 +11,6 @@ const firebaseConfig = {
   measurementId: "G-Q997Y75BR7"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+    __app_id: JSON.stringify('impromptu-v1')
+  }
+})
